0222e66eee29780e82f58f46ca3bfb2491047c81
小林駿太 <shunta@MacBook-Pro.local>
2018-12-15 20:50:08 +0900

    deviseを導入、Userモデルを作成、ログインページとユーザー登録ページを作成

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+  before_action :logged_in_user, only: :index
+
   def index
+  end
+
+  private
+
+  def logged_in_user
+    unless current_user
+      redirect_to new_user_session_url
+    end
   end
 end
