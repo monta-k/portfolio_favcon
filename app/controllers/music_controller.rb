dbc0da9a12570966bf3de922dc3ea427dc4ff344
小林駿太 <shunta@MacBook-Pro.local>
2019-01-02 18:05:55 +0900

    #2

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,4 +1,12 @@
 class MusicController < ApplicationController
   def index
+    if params[:q]
+      @search_word = params[:q]
+      @artist_lists = SearchMusic.search_by_artist(params[:q])
+    elsif params[:artist_id]
+      @album_lists = LookupMusic.search_by_artist(params[:artist_id])
+    elsif params[:album_id]
+      @song_lists = LookupMusic.search_by_album(params[:album_id])
+    end
   end
 end
