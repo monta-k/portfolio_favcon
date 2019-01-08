2a4e75676176281af4d5fa4dfd9b116f69855a37
小林駿太 <shunta@MacBook-Pro.local>
2019-01-08 23:45:36 +0900

    #7 映画ページのトップにも投稿一覧実装

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -3,6 +3,8 @@ class MovieController < ApplicationController
     if params[:q]
       @search_word = params[:q]
       @movie_lists = SearchMovie.search_by_title(params[:q])
+    else
+      @posts = Post.where(track_type: 0)
     end
   end
 end
