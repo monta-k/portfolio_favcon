f862588d57b4aa90066470c0b26413344b5f970c
小林駿太 <shunta@MacBook-Pro.local>
2019-01-11 21:48:40 +0900

    rubocop違反を修正

--- lib/search_music.rb
+++ lib/search_music.rb
@@ -3,7 +3,7 @@ require 'itunes-search-api'
 class SearchMusic
   def self.search_by_artist(artist)
     artist_lists = []
-    search_results = ITunesSearchAPI.search(:term => artist, :country => 'jp', :media   => 'music',:entity => "musicArtist", :attribute => 'artistTerm', :lang    => 'ja_jp', :limit => 200)
+    search_results = ITunesSearchAPI.search(:term => artist, :country => 'jp', :media => 'music', :entity => "musicArtist", :attribute => 'artistTerm', :lang => 'ja_jp', :limit => 200)
     search_results.each do |result|
       artist_lists << result
     end
