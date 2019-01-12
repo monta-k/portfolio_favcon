d5730479d377e3b81395a429f2ee1d6a4735e204
小林駿太 <shunta@MacBook-Pro.local>
2019-01-12 15:13:33 +0900

    #22

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,5 +63,5 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include ControllerMacros, type: :controller
+  config.include ControllerMacros
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,12 +1,15 @@
 module ControllerMacros
-  def login_admin(admin)
-    @request.env["devise.mapping"] = Devise.mappings[:admin]
-    sign_in admin
+  def sign_in(email, password)
+    first("input[type='email']").set(email)
+    first("input[type='password']").set(password)
+    click_button "Log in"
   end
 
-  def login_user(user)
-    @request.env["devise.mapping"] = Devise.mappings[:admin]
-    user.confirm!
-    sign_in user
+  def sign_up(username, email, password, password_confirmation)
+    fill_in "user_username", with: username
+    fill_in "user_email", with: email
+    fill_in "user_password", with: password
+    fill_in "user_password_confirmation", with: password_confirmation
+    click_button "Sign up"
   end
 end
