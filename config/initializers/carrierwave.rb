1893b6fa2c5c4482135383da4f6ad8edd687edb2
小林駿太 <shunta@MacBook-Pro.local>
2019-01-17 18:58:01 +0900

    carrierwave.rbを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,7 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_provider = 'fog/aws'
+    config.asset_host = ENV['URL']
+    config.cache_storage = :fog
   end
 end
